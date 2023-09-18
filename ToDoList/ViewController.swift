1b34e774b30db012f9ea9f4fed82560fd4bb8692
abedsully <abedsully2909@gmail.com>
2023-09-18 11:19:48 +0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Stefanus Albert Wilson on 9/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
